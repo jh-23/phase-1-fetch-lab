56a5ef81f67658fce8d6075cf5769e928cf649fc
John Heger <heger7@gmail.com>
2023-10-25 16:38:38 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      console.log(response)
+      return response.json()
+    })
+    .then(function(data) {
+      console.log(data)
+      renderBooks(data)
+    })
 }
 
 function renderBooks(books) {
